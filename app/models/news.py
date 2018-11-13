1008e4cabf3901a243bc59d61fbb25bb0a2fdbe8
vinnyotach7 <vinnyotach7@gmail.com>
2018-11-13 17:02:59 +0300

    added some input functions to this file

--- app/models/news.py
+++ app/models/news.py
@@ -1,16 +1,16 @@
 class News:
-    def __init__(self,source,link,country,id,description):
-        self.source = source
-        self.link = link
-        self.country = country
-        self.id = id
+    def __init__(self,name,url,description,urlToImage,publishedAt,content):
+        self.name = name
         self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content =content
+        self.url =url
 
 
 class Articles:
     def __init__(self,name,description,url,category):
         self.name = name
         self.description = description
-        self.url = url
         self.url = url
         self.category = category
